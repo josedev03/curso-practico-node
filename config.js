2aca24b9e95d2dc2baedc61708e297dd7db0fced
Rodriguez <joseluis.rodriguez@dxc.com>
2020-07-11 12:17:35 -0500

    variables de entorno vercel

--- config.js
+++ config.js
@@ -9,13 +9,6 @@ module.exports = {
   jwt: {
     secret: process.env.JWT_SECRET || 'notasecret!'
   },
-  mysql: {
-    host: process.env.MYSQL_HOST || 'remotemysql.com',
-    user: process.env.MYSQL_USER || 'Ln4iMlbObf',
-    password: process.env.MYSQL_PASS || '9fM1MC3imd',
-    database: process.env.MYSQL_DB || 'Ln4iMlbObf',
-    port: process.env.MYSQL_PORT || 3306,
-  },
   mysqlService: {
     host: process.env.MYSQL_SRV_HOST || 'localhost',
     port: process.env.MYSQL_SRV_PORT || 3001,
